52c1a4bcf75a9cb04754cd345bd564c0f3d663b2
Matthew Connell <matthew.at.connell@gmail.com>
2018-03-09 16:18:57 +0900

    Some debugging

--- Shiny app prep.R
+++ Shiny app prep.R
@@ -1,7 +1,6 @@
 setwd("C:/Users/matth_000/Desktop/Coursera/Month10-CapstoneProject/final/en_US/Final_tables")
 
-library(quanteda)
-library(readtext)
+
 library(data.table)
 library(sqldf)
 
@@ -39,35 +38,106 @@ fourgrams_splitted$last_word <- as.character(fourgrams_splitted$last_word)
 ##
 ##
 
+## Define finder functions
+
+
+bigram_finder <- function(splitted_string) {
+          new_string = splitted_string
+          finder_two <- grepl(paste0(" ", new_string), bigrams_splitted$first_words, ignore.case = TRUE)
+          getter_two<- bigrams_splitted$last_word[finder_two]
+          return(getter_two[1:4])
+}
+
+
+trigram_finder <- function(splitted_string) {
+          new_string = paste(splitted_string[1:2], collapse = " ")
+          finder_three <- grepl(paste0(" ", new_string), trigrams_splitted$first_words, ignore.case = TRUE)
+          getter_three<- trigrams_splitted$last_word[finder_three]
+          return(getter_three[1:4])
+}
+
+fourgram_finder <- function(splitted_string) {
+          new_string = paste(splitted_string[1:3], collapse = " ")
+          finder_four <- grepl(paste0(" ", new_string), fourgrams_splitted$first_words, ignore.case = TRUE)
+          getter_four<- fourgrams_splitted$last_word[finder_four]
+          return(getter_four[1:4])
+}
+
+
+
+
+
 predictor <- function(string) {
   
+    splitted_string <- unlist(strsplit(string, split = " "))
+    len_string = length(splitted_string)  
   
-  splitted_string <- unlist(strsplit(string, split = " "))
-  len_string = length(splitted_string)  
-  
-  if (len_string >= 3) {
-    new_string = paste(splitted_string[(len_string-2):len_string], collapse = " ")
-    finder_four <- grepl(new_string, fourgrams_splitted$first_words, ignore.case = TRUE)
-    getter_four<- fourgrams_splitted$last_word[finder_four]
-    return(getter_four[1:3])
-    
-    
-  } else if (len_string == 2) {
-    new_string = paste(splitted_string[(len_string-1):len_string], collapse = " ")
-    finder_three <- grepl(new_string, trigrams_splitted$first_words, ignore.case = TRUE)
-    getter_three<- trigrams_splitted$last_word[finder_three]
-    return(getter_three[1:4])
-    
-  } else if (len_string == 1) {
-    new_string = unlist(strsplit(string, " "))
-    finder_two <- grepl(new_string, bigrams_splitted$first_words, ignore.case = TRUE)
-    getter_two<- bigrams_splitted$last_word[finder_two]
-    return(getter_two[1:4])
-    
-    
-    
-  }
+        if (len_string >= 3) {
+                results <- fourgram_finder(splitted_string)
+                if (sum(is.na(results)) == 0) {
+                        return(results)
+                } else if (sum(is.na(results)) == 1) {
+                        results2 <- trigram_finder(splitted_string[2:3])
+                        return(c(results[1:3], results2[1]))
+                } else if (sum(is.na(results)) == 2) {
+                        results3 <- trigram_finder(splitted_string[2:3])
+                        return(c(results[1:2], results3[1:2]))
+                } else if (sum(is.na(results)) == 3) {
+                        results4 <- trigram_finder(splitted_string[2:3])
+                        return(c(results[1], results4[1:3]))
+                } else if (sum(is.na(results)) == 4) {
+                        results5 <- trigram_finder(splitted_string[2:3])
+                        return(results5[1:4])
+                }
+               
+        
+        
+       
+        
+      } else if (len_string == 2) {
+                results <- trigram_finder(splitted_string)
+                if (sum(is.na(results)) == 0) {
+                        return(results)
+                } else if (sum(is.na(results)) == 1) {
+                        results2 <- bigram_finder(splitted_string[2])
+                        return(c(results[1:3], results2[1]))
+                } else if (sum(is.na(results)) == 2) {
+                        results3 <- bigram_finder(splitted_string[2])
+                        return(c(results[1:2], results3[1:2]))
+                } else if (sum(is.na(results)) == 3) {
+                        results4 <- bigram_finder(splitted_string[2])
+                        return(c(results[1], results4[1:3]))
+                } else if (sum(is.na(results)) == 4) {
+                        results5 <- bigram_finder(splitted_string[2])
+                        return(results5[1:4])
+                }
+        
+        
+        
+      } else if (len_string == 1) {
+               results <- bigram_finder(splitted_string)
+               if (sum(is.na(results)) == 0) {
+                       return(results)
+               } else if (sum(is.na(results)) == 1) {
+                       results2 <- bigram_finder(splitted_string[2])
+                       return(c(results[1:3], results2[1]))
+               } else if (sum(is.na(results)) == 2) {
+                       results3 <- bigram_finder(splitted_string[2])
+                       return(c(results[1:2], results3[1:2]))
+               } else if (sum(is.na(results)) == 3) {
+                       results4 <- bigram_finder(splitted_string[2])
+                       return(c(results[1], results4[1:3]))
+               } else if (sum(is.na(results)) == 4) {
+                       results5 <- bigram_finder(splitted_string[2])
+                       return(results5[1:4])
+               } 
+        
+        
+      }
+
 }
+
+
 
 start <- Sys.time()
 predictor("the")
