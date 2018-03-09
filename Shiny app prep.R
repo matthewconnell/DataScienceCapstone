67772e096bec3c5acc6ab43053f2b2bcce216be1
Matthew Connell <matthew.at.connell@gmail.com>
2018-03-10 00:02:09 +0900

    Almost fixed the NA problem. Function a little bit slower now.

--- Shiny app prep.R
+++ Shiny app prep.R
@@ -73,29 +73,102 @@ unigram_finder <- function(number) {
 }
 
 
+
 predictor <- function(string) {
   
     splitted_string <- unlist(strsplit(string, split = " "))
     len_string = length(splitted_string)  
   
         if (len_string >= 3) {
+                splitted_string <- splitted_string[(len_string - 2):len_string]
                 results <- fourgram_finder(splitted_string)
-                if (sum(is.na(results)) == 0) {
-                        return(results)
-                } else if (sum(is.na(results)) == 1) {
+                
+                if (sum(is.na(results[1:4])) == 0) {
+                        return(results[1:4])
+                        
+                } else if (sum(is.na(results[1:4])) == 1) {
                         results2 <- trigram_finder(splitted_string[2:3])
-                        return(c(results[1:3], results2[1]))
-                } else if (sum(is.na(results)) == 2) {
+                        if (sum(is.na(results2[1])) == 0) {
+                                return(c(results[1:3], results2[1]))
+                        } else if (sum(is.na(results2[1])) == 1) {
+                                results2_sub <- bigram_finder(splitted_string[3])
+                                if (sum(is.na(results2_sub[1])) == 0) {
+                                        return(c(results[1:3], results2_sub[1]))
+                                } else {
+                                        results2_sub_sub <- unigram_finder(1)
+                                        return(c(results[1:3]), results2_sub_sub[1])
+                                }
+                        } 
+                } else if (sum(is.na(results[1:4])) == 2) {
                         results3 <- trigram_finder(splitted_string[2:3])
-                        return(c(results[1:2], results3[1:2]))
-                } else if (sum(is.na(results)) == 3) {
-                        results4 <- trigram_finder(splitted_string[2:3])
-                        return(c(results[1], results4[1:3]))
-                } else if (sum(is.na(results)) == 4) {
-                        results5 <- trigram_finder(splitted_string[2:3])
-                        return(results5[1:4])
+                        if (sum(is.na(results3[1:2])) == 0) {
+                                return(c(results[1:2], results3[1:2]))
+                        } else if (sum(is.na(results3[1:2])) == 1) {
+                                results3_sub <- bigram_finder(splitted_string[3])
+                                if (sum(is.na(results3_sub)) == 0) {
+                                        return(c(results[1:2], results3_sub[1:2]))
+                                } else {
+                                        results3_sub_sub <- unigram_finder(2)
+                                        return(c(results[1:2]), results3_sub_sub[1:2])
+                                }
+                        }
                 }
-               
+                
+                        
+                else if (sum(is.na(results[1:4])) == 3) {
+                        results4 <- trigram_finder(splitted_string[2:3])
+                        if (sum(is.na(results4[1:3])) == 0) {
+                                return(c(results4[1], results4[1:3]))
+                        } else if (sum(is.na(results4[1:3])) == 1) {
+                                results4_sub <- bigram_finder(splitted_string[3])
+                                if (sum(is.na(results4_sub)) == 0) {
+                                        return(c(results[1], results4_sub[1:3]))
+                                } else {
+                                        results4_sub_sub <- unigram_finder(3)
+                                        return(c(results[1]), results4_sub_sub[1:3])
+                                }
+                        } 
+                }
+                
+                else if (sum(is.na(results[1:4])) == 4) {
+                        results5 <- trigram_finder(splitted_string[2:3])
+                        if (sum(is.na(results5[1:4])) == 0) {
+                                return(results5[1:4])
+                                
+                        } else if (sum(is.na(results5[1:4])) == 1) {
+                                results5_sub <- bigram_finder(splitted_string[3])
+                                if (sum(is.na(results5_sub)) == 0) {
+                                        return(results5_sub[1:4])
+                                } else {
+                                        results5_sub_sub <- unigram_finder(4)
+                                        return(c(results5[1:3], results5_sub_sub[1]))
+                                }
+                        } else if (sum(is.na(results5[1:4])) == 2) {
+                                results6_sub <- bigram_finder(splitted_string[3])
+                                if (sum(is.na(results6_sub)) == 0) {
+                                        return(c(results5[1:2], results5_sub[1:2]))
+                                } else {
+                                        results6_sub_sub <- unigram_finder(2)
+                                        return(c(results5[1:2], results6_sub_sub[1:4]))
+                                }
+                        } else if (sum(is.na(results5[1:4])) == 3) {
+                                results7_sub <- bigram_finder(splitted_string[3])
+                                if (sum(is.na(results7_sub)) == 0) {
+                                        return(c(results5[1], results7_sub[1:3]))
+                                } else {
+                                        results7_sub_sub <- unigram_finder(4)
+                                        return(c(results5[1], results7_sub_sub[1]))
+                                } 
+                        } else if (sum(is.na(results5[1:4])) == 4) {
+                                results8_sub <- bigram_finder(splitted_string[3])
+                                if (sum(is.na(results8_sub)) == 0) {
+                                        return(results8_sub[1:4])
+                                } else {
+                                        results8_sub_sub <- unigram_finder(4)
+                                        return(results8_sub_sub[1:4])
+                                }
+                        }
+                }
         
         
        
@@ -144,23 +217,6 @@ predictor <- function(string) {
 }
 
 
-replace_i <- function(column) {
-        column <- gsub("^i$", "I", column)
-        column <- gsub("^i'm$", "I'm", column)
-        return(column)
-}
-
-
-
-unigrams$unique.values <- replace_i(unigrams$unique.values)
-bigrams_splitted$last_word <- replace_i(bigrams_splitted$last_word)
-trigrams_splitted$last_word <- replace_i(trigrams_splitted$last_word)
-fourgrams_splitted$last_word <- replace_i(fourgrams_splitted$last_word)
-
-write.table(unigrams, "unigrams.txt")
-write.table(bigrams_splitted, "bigrams_splitted.txt")
-write.table(trigrams_splitted, "trigrams_splitted.txt")
-write.table(fourgrams_splitted, "fourgrams_splitted.txt")
 
 
 ## Find data
