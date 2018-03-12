7ba42496a722918576b2b35dd33523d0ccee7b1e
Matthew Connell <matthew.at.connell@gmail.com>
2018-03-12 19:54:49 +0900

    fixed NA error issue

--- Shiny app prep.R
+++ Shiny app prep.R
@@ -40,36 +40,67 @@ fourgrams_splitted$last_word <- as.character(fourgrams_splitted$last_word)
 
 ## Define finder functions
 
+unigram_finder <- function(number) {
+        random_words <- c()
+        words <- unigrams$unique.values[1:number]
+        return(words)
+}
+
 
 bigram_finder <- function(splitted_string) {
           new_string = splitted_string
           finder_two <- grepl(paste0("\\<", new_string, "\\>"), bigrams_splitted$first_words, ignore.case = TRUE)
           getter_two<- bigrams_splitted$last_word[finder_two]
+          sum_na <- sum(is.na(getter_two[1:4]))
+          if (sum_na < 4)  {
+                          getter_two <- c(getter_two[1:(4-sum_na)], unigram_finder(sum_na))
+                          return(getter_two[1:4])
+          } else {
+                  getter_two <- unigram_finder(sum_na)
+                  return(getter_two[1:4])
+          } 
           return(getter_two[1:4])
-}
+
+                  }
 
 
 trigram_finder <- function(splitted_string) {
           new_string = paste(splitted_string[1:2], collapse = " ")
           finder_three <- grepl(paste0("\\<", new_string, "\\>"), trigrams_splitted$first_words, ignore.case = TRUE)
           getter_three<- trigrams_splitted$last_word[finder_three]
-          return(getter_three[1:4])
+          sum_na <- sum(is.na(getter_three[1:4]))
+          if (sum_na < 4)  {
+                  getter_three <- c(getter_three[1:(4-sum_na)], bigram_finder(splitted_string[2]))
+                  return(getter_three[1:4])
+          } else {
+                  getter_three <- bigram_finder(splitted_string[2])
+                  return(getter_three[1:4])
+          } 
+
 }
+
+
+
+
+
+
 
 fourgram_finder <- function(splitted_string) {
           new_string = paste(splitted_string[1:3], collapse = " ")
           finder_four <- grepl(paste0("\\<", new_string, "\\>"), fourgrams_splitted$first_words, ignore.case = TRUE)
           getter_four<- fourgrams_splitted$last_word[finder_four]
-          return(getter_four[1:4])
+          sum_na <- sum(is.na(getter_four[1:4]))
+          if (sum_na < 4)  {
+                  getter_four <- c(getter_four[1:(4-sum_na)], trigram_finder(splitted_string[2:3]))
+                  return(getter_four[1:4])
+          } else {
+                  getter_four <- trigram_finder(splitted_string[2:3])
+                  return(getter_four[1:4])
+          } 
+          
 }
 
-unigram_finder <- function(number) {
-        random_words <- c()
-        for (i in 1:number) {
-                words <- unigrams$unique.values[1:i]
-        }
-        return(words)
-}
+
 
 
 
--- bcl/app.R
+++ bcl/app.R
@@ -1,10 +1,28 @@
 
 library(shiny)
+library(ggplot2)
+library(dplyr)
 
 ui <- fluidPage(titlePanel("BC Liquor Store Prices"),
                 sidebarLayout(
-                        sidebarPanel("inputs"),
-                        mainPanel("results here")
+                        sidebarPanel(
+                                sliderInput(inputId = "priceInput",
+                                            label = "Price",
+                                            min = 0,
+                                            max = 100,
+                                            value = c(25,40),
+                                            pre = "$"),
+                                radioButtons(inputId = "typeInput",
+                                             label ="Product type",
+                                             choices = c("BEER", "REFRESHMENT", "SPIRITS", "WINE"),
+                                             selected = "WINE"),
+                                uiOutput("countryOutput")
+                        ),
+                        mainPanel(
+                                plotOutput("coolplot"),
+                                br(), br(),
+                                DT::dataTableOutput("results")
+                        )
                 
                 )
                 )
@@ -13,7 +31,48 @@ ui <- fluidPage(titlePanel("BC Liquor Store Prices"),
 
 
 server <- function(input, output) {
-   
+        
+        
+        filtered <- reactive({
+                if (is.null(input$countryInput)) {
+                        return(NULL)
+                }
+                bcl %>%
+                filter(Price >= input$priceInput[1],
+                       Price <= input$priceInput[2],
+                       Type == input$typeInput, 
+                       Country == input$countryInput
+                )
+        })
+        
+        
+        output$coolplot <- renderPlot({
+                if (is.null(filtered())) {
+                        return()
+                }
+                ggplot(filtered(), aes(Alcohol_Content)) + 
+                        geom_histogram()
+        })
+        
+        
+        output$results <- DT::renderDataTable({
+                filtered() 
+        })
+        
+        
+        output$countryOutput <- renderUI({
+                selectInput("countryInput", "Country",
+                            sort(unique(bcl$Country)),
+                            selected = "CANADA")
+        })
+        
+        priceDiff <- reactive({
+                input$priceInput
+        })
+        
+        observe({ print(priceDiff()) })
+        
+        
    
 }
 
