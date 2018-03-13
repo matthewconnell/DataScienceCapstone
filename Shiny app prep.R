6afcffbbf23c6a13146e2606ee3ad4252b0df366
Matthew Connell <matthew.at.connell@gmail.com>
2018-03-13 18:04:40 +0900

    started documentation

--- Shiny app prep.R
+++ Shiny app prep.R
@@ -164,7 +164,7 @@ output$prediction4 <- renderText({predictor(input$text)}[4])
 ## Shiny themes https://gallery.shinyapps.io/117-shinythemes/
 ## Shiny themes again http://rstudio.github.io/shinythemes/
 ## Source for wordcloud help http://www.sthda.com/english/wiki/text-mining-and-word-cloud-fundamentals-in-r-5-simple-steps-you-should-know
-## 
+## Source for plot output https://shiny.rstudio.com/reference/shiny/latest/plotOutput.html
 
 
 
--- text_predictor/ui.R
+++ text_predictor/ui.R
@@ -57,14 +57,47 @@ shinyUI(fluidPage(theme = shinytheme("simplex"),
                                                 column(width = 2, align = "right", offset = 0)
                                                 )
                         ),
-                        tabPanel("Documentation", 
-                                 "place holding text"),
+                        tabPanel("Documentation",
+                                 sidebarLayout(
+                                         sidebarPanel(
+                                         ),
+                                         mainPanel(
+                                               fluidRow(column = 8,
+                                          offset = 5,
+                                          align = "left",
+                                          tags$h1("Documentation"),
+                                         br(),
+                                         tags$h2("About the app"),
+                                         br(),
+                                         tags$h4("This app uses an algorithm based on the Katz's Backoff Model of word prediction.
+                                                 It works by taking the most recent three words and finding the most likely next four words.
+                                                 If it cannot find a suitable word, it will look at the previous two words and try to predict based on them.
+                                                 Similarly, if it cannot find a word based on those two, it will look at the individual most recent word typed.
+                                                 When it cannot find any good words at all, it returns one of the top four most common words.
+                                                 If there are dupicates in the returned word list, a placeholder word is used."),
+                                         tags$h2("Limitations"),
+                                         tags$h4("The application has been built for speed. 
+                                                 In the case of some n-gram searches, this will result some accuracy loss. 
+                                                 This is due to the file size limitations.
+                                                 Only 20% of the data was sampled and of that, the least common n-gram combinations were dropped from the table. 
+                                                 "),
+                                                br(),
+                                         tags$h4("Furthermore, the build is currently only available for the English language."),
+                                         tags$h2("Next Steps"),
+                                         tags$h4("The next steps would be to add another language. 
+                                                 Russian or German would be the best choices as the data has already been made available as part of this project."),
+                                         tags$h2("Sources")  
+                                         )
+                                 )
+                                 
+                        )
+                        ),
                         
                         
                         
                         
                         
-                        tabPanel("Visualizations", 
+                        tabPanel("Visualization", 
                                  tags$h1("Word cloud"),
                                  sidebarLayout(
                                          sidebarPanel(
